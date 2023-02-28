82e4b17dbe6c25c654b7e70751d50d53328651fc
EmiNiemand <Szymon_PL@live.com>
2023-02-28 09:12:24 +0100

    fix plant

--- Assets/Scripts/Enemies/Plant.cs
+++ Assets/Scripts/Enemies/Plant.cs
@@ -9,28 +9,24 @@ namespace Enemies
     {
         private GameObject missile;
         [SerializeField] private GameObject missilePrefab;
-
+        [SerializeField] private float distanceToActivate;
+        private GameObject player;
+        
         // Start is called before the first frame update
         protected override void Start()
         {
             base.Start();
+            player = GameObject.FindWithTag("Player");
         }
 
         // Update is called once per frame
         void Update()
         {
-        
-        }
-
-        private void OnTriggerStay2D(Collider2D other)
-        {
-            if (missile != null) return;
-            if (other && other.CompareTag("Player"))
-            {
-                missile = Instantiate(missilePrefab, transform.position + 
-                                                     new Vector3(spriteRenderer.size.x / 2 * lookingDirection, spriteRenderer.size.y / 4, 0), Quaternion.identity);
-                missile.transform.SetParent(gameObject.transform);
-            }
+            if (Mathf.Abs(Vector2.Distance(player.transform.position, transform.position)) > distanceToActivate) return;
+            if (missile) return;
+            missile = Instantiate(missilePrefab, transform.position + 
+                                                 new Vector3(spriteRenderer.size.x / 2 * lookingDirection, spriteRenderer.size.y / 4, 0), Quaternion.identity);
+            missile.transform.SetParent(gameObject.transform);
         }
     }
 }
