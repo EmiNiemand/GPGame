ba09ea5be6566329747c8c78f1f683144eff092e
Michalina Śledzikowska <236669@edu.p.lodz.pl>
2023-06-14 18:11:35 +0200

    Improve IUsable documentation

--- Assets/Scripts/Interfaces/IUsable.cs
+++ Assets/Scripts/Interfaces/IUsable.cs
@@ -3,6 +3,10 @@ using UnityEngine;
 /// <summary>
 ///     Player can interact with/use object implementing this interface
 ///</summary>
+/// <remarks>
+///     GameObject using script implementing this interface must have
+///     Collider2D set to Trigger as well as be on <b>Usable</b> layer
+/// </remarks>
 public interface IUsable {
     void OnEnter(GameObject user);
     void OnExit(GameObject user);
